405d2827e0d04a20ffd3db04ab1e134d50466680
wanpeng16 <wanpengwx@163.com>
2016-11-23 22:42:33 +0800

    增加注释

--- search_tree.py
+++ search_tree.py
@@ -12,10 +12,13 @@ def insert(node,key,val):
         node.right = insert(node.right,key,val)
     return node
 
+
+#递归描述
+#每次经过两次比较进入对应的分支
 def search(node,key):
-    if node is None:raise KeyError
-    if node.key == key:return node.val
-    elif key < node.key:
+    if node is None:raise KeyError #叶节点的分支，抛异常
+    if node.key == key:return node.val #返回对应的值
+    elif key < node.key:  #左分支
         return search(node.left,key)
-    else:
+    else:                 #右分支
         return  search(node.right,key)
