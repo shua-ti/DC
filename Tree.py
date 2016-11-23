a51a9e88d17bea38370c452efd4f720461d1e942
wanpeng16 <wanpengwx@163.com>
2016-11-23 22:26:23 +0800

    搜索树定义

---
+++ Tree.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+#/usr/bin/env python
+"搜索树定义"
+from search_tree import *
+class Tree:
+    root = None
+    def __setitem__(self, key, value):
+        self.root = insert(self.root,key,val)
+    def __getitem__(self, key):
+        return search(self.root,key)
+    def __contains__(self, key):
+        try: search(self.root,key)
+        except KeyReeor:return false
+        return true
